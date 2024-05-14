aac4178fd0966520846a23ae1489b72909b445c2
Karol S <karol96266@gmail.com>
2024-05-14 13:12:17 +0000

    Add about page

---
+++ my-app/app/about/page.js
@@ -0,0 +1,9 @@
+export default function About() {
+    return (
+      <div>
+        <h1>About Page</h1>
+        <p>This is a simple about page to understand routing in Next.js.</p>
+      </div>
+    );
+  }
+  
